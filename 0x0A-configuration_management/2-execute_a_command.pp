c77fe56fd3048ef412c0be4a48665691048e43a4
Cyn <cmdominguez@gmail.com>
2019-08-06 06:10:59 +0000

    Refactor to streamline manifest

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,7 +2,5 @@
 
 exec { 'pkill -f':
   command => 'pkill -f killmenow',
-  cwd     => '/vagrant/Documents/holberton-system_engineering-devops/0x0A-configuration_management/',
-  creates => '/0x0A-configuration_management/killmenow',
   path    => [ '/usr/bin/' ],
 }
