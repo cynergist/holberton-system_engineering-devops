fbea14c3034ed53dce2518f5b0a02d4a08f6475b
Cyn <cmdominguez@gmail.com>
2019-08-06 06:09:01 +0000

    Add command for pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,8 @@
 # This Puppet manifest kills a process named killmenow
 
-exec { 'pkill -f killmenow':
+exec { 'pkill -f':
+  command => 'pkill -f killmenow',
+  cwd     => '/vagrant/Documents/holberton-system_engineering-devops/0x0A-configuration_management/',
   creates => '/0x0A-configuration_management/killmenow',
-  path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+  path    => [ '/usr/bin/' ],
 }
