fbb3928f6cbbb3b1c3961e3974bda61e05c671eb
Cyn <cmdominguez@gmail.com>
2019-09-17 06:07:19 +0000

    add 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,19 +2,39 @@
 """A script, using the API at https://jsonplaceholder.typicode.com to
 return info about a given employee's TODO list progress.
 """
-import requests
+
 import json
+import requests
+from sys import argv
 
+if __name__ == "__main__":
+    '''Set empty todo_list to collect todo items'''
+    todo_list = []
+    '''Users URL adds a new num to the path, which will be line arg 1'''
+    users_url = 'https://jsonplaceholder.typicode.com/users/'
+    user_id = argv[1]
+    users_r = requests.get(users_url + user_id)
+    user = users_r.json()
+    '''Todos URL adds a new num to the path for each userId: line arg 1'''
+    todos_url = 'https://jsonplaceholder.typicode.com/todos?userId='
+    todo_id = argv[1]
+    todos_r = requests.get(todos_url + todo_id)
+    todos = todos_r.json()
+    '''Search for 'completed' key, add 'title' of the todo to our todo_list'''
+    for everything in todos:
+        if everything.get('completed'):
+            todo_list.append(everything.get('title'))
+    '''Search for the 'name' key so we can display the employee's name'''
+    for everything in todos:
+        name = user.get('name')
+    '''Count up the tasks collected in the todo_list with totals'''
+    how_many_tasks = len(todo_list)
+    total_tasks = len(todos)
 
-url = 'https://jsonplaceholder.typicode.com/todos/'
-response = requests.get(
-    url,
-    params ={"userId": 1}
-)
-todos = response.json()
-print(todos)
-print(response.url)
-
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'), how_many_tasks, total_tasks))
+    for everything in todo_list:
+        print("\t" + everything)
 '''
 Example of executed script:
 $ python3 0-gather_data_from_an_API.py 4
@@ -25,5 +45,4 @@ Employee Patricia Lebsack is done with tasks(6/20):
      sequi dolorem sed
      eum ipsa maxime ut
      tempore molestias dolores rerum sequi voluptates ipsum consequatur
-
 '''
